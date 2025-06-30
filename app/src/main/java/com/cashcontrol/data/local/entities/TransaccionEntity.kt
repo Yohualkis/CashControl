ded9f85e5d9c7b3194dd658cbea4032f3f20c0c6
Yohualkis <Yohualkis99@gmail.com>
2025-06-30 02:29:01 -0400

    feat(entities): Se creo la entidad

---
+++ app/src/main/java/com/cashcontrol/data/local/entities/TransaccionEntity.kt
@@ -0,0 +1,31 @@
+package com.cashcontrol.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(
+    tableName = "Transacciones",
+    foreignKeys = [
+        ForeignKey(
+            entity = CategoriaEntity::class,
+            parentColumns = ["categoriaId"],
+            childColumns = ["categoriaId"],
+            onDelete = ForeignKey.CASCADE,
+        )
+    ]
+)
+data class TransaccionEntity(
+    @PrimaryKey
+    val transaccionId: Int?,
+    val usuarioId: Int?,
+    val categoriaId: Int?,
+    val tipoTransaccion: String,
+    val monto: Double,
+    val fechaTransaccion: Date?,
+    val usarSugerencia: Boolean,
+    val descripcion: String,
+    val updatedAt: Long, // timestamp Unix ms de ultima actualizacion
+    val isDirty: Boolean = false // true si esta modificado localmente y no sincronizado
+)
