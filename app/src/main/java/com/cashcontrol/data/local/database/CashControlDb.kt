b7dfeb2047dc1cb9fafd7b6b717c30699472dec9
Yohualkis <Yohualkis99@gmail.com>
2025-06-30 02:37:21 -0400

    refactor(database): Se optimizo un import en CashControlDb

--- app/src/main/java/com/cashcontrol/data/local/database/CashControlDb.kt
+++ app/src/main/java/com/cashcontrol/data/local/database/CashControlDb.kt
@@ -3,10 +3,9 @@ package com.cashcontrol.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.cashcontrol.Converters
 import com.cashcontrol.data.local.dao.TransaccionDao
-import com.cashcontrol.data.local.entities.TransaccionEntity
 import com.cashcontrol.data.local.entities.CategoriaEntity
+import com.cashcontrol.data.local.entities.TransaccionEntity
 
 @Database(
     entities = [
