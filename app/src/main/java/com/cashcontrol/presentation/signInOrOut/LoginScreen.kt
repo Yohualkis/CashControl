6ae57824137ee798e1c3bebc5ea73daedbfdfded
Yohualkis <Yohualkis99@gmail.com>
2025-06-30 02:29:37 -0400

    feat(refactor): Se mejoro el aspecto de la pantalla

--- app/src/main/java/com/cashcontrol/presentation/signInOrOut/LoginScreen.kt
+++ app/src/main/java/com/cashcontrol/presentation/signInOrOut/LoginScreen.kt
@@ -1,30 +1,44 @@
 package com.cashcontrol.presentation.signInOrOut
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Button
-//noinspection UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries,UsingMaterialAndMaterial3Libraries
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.ButtonDefaults
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Divider
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Icon
 //noinspection UsingMaterialAndMaterial3Libraries
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.IconButton
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.OutlinedButton
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.OutlinedTextField
 //noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Text
+//noinspection UsingMaterialAndMaterial3Libraries
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,113 +48,231 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.cashcontrol.R
 
-@Preview(showBackground = true)
 @Composable
-fun LoginScreen() {
+fun LoginScreen(
+    nav: NavHostController
+) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var showPass by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.spacedBy(48.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Spacer(modifier = Modifier.height(64.dp))
-
-        // Título
-        Text(
-            text = "Cash Control",
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            fontWeight = FontWeight.Bold
-        )
-
         Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .background(Color(0xFF016FCE)),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = "Iniciar Sesión",
-                style = MaterialTheme.typography.body1,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = "Introduzca sus credenciales para continuar",
-                style = MaterialTheme.typography.body2,
-                color = Color.Gray
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Email
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("email@domain.com") },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            // Password
-            OutlinedTextField(
-                value = password,
-                onValueChange = { password = it },
-                label = { Text("contraseña") },
-                singleLine = true,
-                visualTransformation =  VisualTransformation.None,
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp)
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            // Botón Continuar
-            Button(
-                onClick = { /* acción de login */ },
-                modifier = Modifier.fillMaxWidth(),
-                colors = ButtonDefaults.buttonColors(contentColor = Color.Black),
+                    .fillMaxSize(),
+                contentAlignment = Alignment.BottomCenter
             ) {
-                Text("Continuar", color = Color.White)
-            }
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            // Separador
-            Divider(thickness = 0.5.dp)
-            Text("o", modifier = Modifier.align(Alignment.CenterHorizontally))
-            Divider(thickness = 0.5.dp)
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            // Google Sign-In Button
-            OutlinedButton(
-                onClick = { /* login con Google */ },
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.google_logo32), // usa el nombre correcto
-                    contentDescription = "Google",
-                    modifier = Modifier.size(18.dp)
+                Image(
+                    painter = painterResource(R.drawable.cashcontrol_logo),
+                    contentDescription = "logo",
+                    modifier = Modifier
+                        .size(120.dp)
                 )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text("Continuar con Google")
             }
-
         }
 
-        // Footer
-        Text(
-            text = "© 2025 Cash Control | TE. Development",
-            style = MaterialTheme.typography.caption,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            color = Color.Gray
-        )
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .background(
+                    MaterialTheme.colorScheme.surfaceDim
+                ),
+            verticalArrangement = Arrangement.spacedBy(48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Column(
+                modifier = Modifier.padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.login),
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+                Text(
+                    text = stringResource(R.string.subtitulo_login),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.outline
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Email
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text(stringResource(R.string.placeholder_email)) },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = TextFieldDefaults.run {
+                        textFieldColors(
+                            cursorColor = MaterialTheme.colorScheme.primary,
+                            focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+                            focusedLabelColor = MaterialTheme.colorScheme.primary,
+                            textColor = MaterialTheme.colorScheme.scrim,
+                            backgroundColor = MaterialTheme.colorScheme.surface,
+                            errorCursorColor = MaterialTheme.colorScheme.error,
+                            errorIndicatorColor = MaterialTheme.colorScheme.error,
+                            leadingIconColor = MaterialTheme.colorScheme.outline,
+                            errorLeadingIconColor = MaterialTheme.colorScheme.error,
+                            errorTrailingIconColor = MaterialTheme.colorScheme.error,
+                            errorLabelColor = MaterialTheme.colorScheme.error,
+                            placeholderColor = MaterialTheme.colorScheme.outline,
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Email,
+                            contentDescription = "Icono de email"
+                        )
+                    }
+                )
+
+                // Password
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text(stringResource(R.string.placeholder_contraseña)) },
+                    singleLine = true,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = TextFieldDefaults.run {
+                        textFieldColors(
+                            cursorColor = MaterialTheme.colorScheme.primary,
+                            focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+                            focusedLabelColor = MaterialTheme.colorScheme.primary,
+                            textColor = MaterialTheme.colorScheme.scrim,
+                            backgroundColor = MaterialTheme.colorScheme.surface,
+                            errorCursorColor = MaterialTheme.colorScheme.error,
+                            errorIndicatorColor = MaterialTheme.colorScheme.error,
+                            leadingIconColor = MaterialTheme.colorScheme.outline,
+                            errorLeadingIconColor = MaterialTheme.colorScheme.error,
+                            errorTrailingIconColor = MaterialTheme.colorScheme.error,
+                            errorLabelColor = MaterialTheme.colorScheme.error,
+                            placeholderColor = MaterialTheme.colorScheme.outline,
+                        )
+                    },
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Lock,
+                            contentDescription = "Icono de clave"
+                        )
+                    },
+                    visualTransformation = if (showPass) VisualTransformation.None else PasswordVisualTransformation(),
+                    trailingIcon = {
+                        val icon = if (showPass) Icons.Default.VisibilityOff else Icons.Default.Visibility
+                        IconButton(onClick = { showPass = !showPass }) {
+                            Icon(icon, contentDescription = null)
+                        }
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Botón Continuar
+                Button(
+                    onClick = { /* acción de login */ },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = MaterialTheme.colorScheme.onPrimary,
+                        backgroundColor = MaterialTheme.colorScheme.primary,
+                    ),
+                    shape = MaterialTheme.shapes.small,
+                ) {
+                    Text(stringResource(R.string.btn_continuar), color = Color.White)
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // Separador
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Divider(
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(1.dp),
+                        color = MaterialTheme.colorScheme.outline
+                    )
+                    Text(
+                        text = stringResource(R.string.otra_opcion),
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                        color = MaterialTheme.colorScheme.outline
+                    )
+                    Divider(
+                        modifier = Modifier
+                            .weight(1f)
+                            .height(1.dp),
+                        color = MaterialTheme.colorScheme.outline
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                // Google Sign-In Button
+                OutlinedButton(
+                    onClick = { /* login con Google */ },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.small,
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = MaterialTheme.colorScheme.primary,
+                        backgroundColor = MaterialTheme.colorScheme.surface
+                    )
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.google_logo32), // usa el nombre correcto
+                        contentDescription = "Google",
+                        modifier = Modifier.size(18.dp),
+                        tint = Color.Unspecified
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(stringResource(R.string.btn_continuar_google))
+                }
+
+            }
+
+            // Footer
+            Text(
+                text = stringResource(R.string.copyright),
+                style = MaterialTheme.typography.labelMedium,
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                color = MaterialTheme.colorScheme.outline
+            )
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLogin() {
+    LoginScreen(
+        nav = rememberNavController()
+    )
 }
