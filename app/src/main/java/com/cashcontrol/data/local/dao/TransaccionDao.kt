f6d56f9b44c60636273e6a78b2779c29f00bc5dc
Yohualkis <Yohualkis99@gmail.com>
2025-06-30 02:28:49 -0400

    feat(dao): Se creo el dao

---
+++ app/src/main/java/com/cashcontrol/data/local/dao/TransaccionDao.kt
@@ -0,0 +1,28 @@
+package com.cashcontrol.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.cashcontrol.data.local.entities.TransaccionEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+sealed interface TransaccionDao {
+    @Upsert
+    suspend fun save(transaccion: TransaccionEntity)
+
+    @Query("""
+        select *
+        from Transacciones
+        where transaccionId = :id
+        limit 1
+    """)
+    suspend fun find(id: Int?): TransaccionEntity?
+
+    @Delete
+    suspend fun delete(transaccion: TransaccionEntity)
+
+    @Query("select * from Transacciones")
+    fun getAll(): Flow<List<TransaccionEntity?>>
+}
