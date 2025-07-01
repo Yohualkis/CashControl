fa7130fea34b67fbaffbf5254ad4589e854941f9
Yohualkis <Yohualkis99@gmail.com>
2025-06-30 20:18:45 -0400

    feat(entities): se creo la entidad SugerenciaDetalleConCategoriaEntity.kt

---
+++ app/src/main/java/com/cashcontrol/data/local/entities/SugerenciaDetalleConCategoriaEntity.kt
@@ -0,0 +1,14 @@
+package com.cashcontrol.data.local.entities
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class SugerenciaDetalleConCategoriaEntity(
+    @Embedded val detalle: SugerenciaDetalleEntity,
+
+    @Relation(
+        parentColumn = "categoriaId",
+        entityColumn = "categoriaId"
+    )
+    val categoria: CategoriaEntity
+)
