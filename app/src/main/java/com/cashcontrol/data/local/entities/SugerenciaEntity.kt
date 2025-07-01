457e611dfe6baa2711b2ccdc2016cad27293d25c
Yohualkis <Yohualkis99@gmail.com>
2025-06-30 22:26:28 -0400

    refactor(entities): se linkeao la tabla sugerencias con la de usuarios y se agrego el decorador primarykey

--- app/src/main/java/com/cashcontrol/data/local/entities/SugerenciaEntity.kt
+++ app/src/main/java/com/cashcontrol/data/local/entities/SugerenciaEntity.kt
@@ -1,10 +1,23 @@
 package com.cashcontrol.data.local.entities
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
 import java.util.Date
 
-@Entity(tableName = "Sugerencias")
+@Entity(
+    tableName = "Sugerencias",
+    foreignKeys = [
+        ForeignKey(
+            entity = UsuarioEntity::class,
+            parentColumns = ["usuarioId"],
+            childColumns = ["usuarioId"],
+            onDelete = ForeignKey.CASCADE,
+        )
+    ]
+)
 data class SugerenciaEntity(
+    @PrimaryKey
     val sugerenciaId: Int?,
     val usuarioId: Int?,
     val estaVisto: Boolean = false,
