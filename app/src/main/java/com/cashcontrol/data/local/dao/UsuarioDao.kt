65fc10b6cc3bf5029e03db3a3e7bb72cbf16d48a
Yohualkis <Yohualkis99@gmail.com>
2025-06-30 22:27:04 -0400

    feat(dao): se creo el dao de usuarios

---
+++ app/src/main/java/com/cashcontrol/data/local/dao/UsuarioDao.kt
@@ -0,0 +1,27 @@
+package com.cashcontrol.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.cashcontrol.data.local.entities.UsuarioEntity
+
+@Dao
+sealed interface UsuarioDao {
+    @Upsert
+    suspend fun save(user: UsuarioEntity)
+
+    @Query("""
+        select *
+        from Usuarios
+        where usuarioId = :id
+    """)
+    suspend fun getUsuarioById(id: Int?): UsuarioEntity?
+
+    @Query("""
+        select *
+        from Usuarios
+        where email = :email
+    """)
+    suspend fun getUsuarioByEmail(email: String): UsuarioEntity?
+}
