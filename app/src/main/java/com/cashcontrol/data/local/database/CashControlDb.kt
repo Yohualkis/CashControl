106a8feabd352530e6f6c923199780f2b327d505
Yohualkis <Yohualkis99@gmail.com>
2025-06-30 20:15:28 -0400

    feat(database): se creo la funcion abstracta que hereda del dao categoria

--- app/src/main/java/com/cashcontrol/data/local/database/CashControlDb.kt
+++ app/src/main/java/com/cashcontrol/data/local/database/CashControlDb.kt
@@ -3,6 +3,7 @@ package com.cashcontrol.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.cashcontrol.data.local.dao.CategoriaDao
 import com.cashcontrol.data.local.dao.TransaccionDao
 import com.cashcontrol.data.local.entities.CategoriaEntity
 import com.cashcontrol.data.local.entities.TransaccionEntity
@@ -18,4 +19,5 @@ import com.cashcontrol.data.local.entities.TransaccionEntity
 @TypeConverters(Converters::class)
 abstract class CashControlDb: RoomDatabase() {
     abstract fun TransaccionDao(): TransaccionDao
+    abstract fun CategoriaDao(): CategoriaDao
 }
