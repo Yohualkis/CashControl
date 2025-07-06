e40602e335713dbe10349de524822f7edec57d9e
Yohualkis <Yohualkis99@gmail.com>
2025-07-06 00:26:29 -0400

    refactor(apis): Se cambio el retorno de algunos metodos y se elimino el comentario

--- app/src/main/java/com/cashcontrol/data/remote/api/CategoriaApi.kt
+++ app/src/main/java/com/cashcontrol/data/remote/api/CategoriaApi.kt
@@ -1,8 +1,6 @@
 package com.cashcontrol.data.remote.api
 
-import com.cashcontrol.data.remote.Resource
 import com.cashcontrol.data.remote.dto.CategoriaDto
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
@@ -11,21 +9,17 @@ import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface CategoriaApi {
-    // OJO: Como los metodos devuelven un "Response<T>" recordar
-    //      que se debe validar manualmente si la respuesta fue
-    //      exitosa (response.isSuccessful) o si hubo un error.
-
     @GET("api/categorias/{usuarioId}")
-    suspend fun getCategorias(@Path("usuarioId") usuarioId: Long): Response<List<CategoriaDto>>
+    suspend fun getCategorias(@Path("usuarioId") usuarioId: Long): List<CategoriaDto>
 
     @GET("api/categorias/{usuarioId}/{categoriaId}")
     suspend fun getCategoriaById(@Path("usuarioId") usuarioId: Long, @Path("categoriaId") categoriaId: Long): CategoriaDto
 
     @POST("api/categorias/{usuarioId}")
-    suspend fun postCategoria(@Path("usuarioId") usuarioId: Long, @Body categoria: CategoriaDto): Resource<CategoriaDto>
+    suspend fun postCategoria(@Path("usuarioId") usuarioId: Long, @Body categoria: CategoriaDto): CategoriaDto
 
     @PUT("api/categorias/{usuarioId}/{categoriaId}")
-    suspend fun putCategoria(@Path("usuarioId") usuarioId: Long, @Path("categoriaId") categoriaId: Long, @Body categoria: CategoriaDto): Resource<CategoriaDto>
+    suspend fun putCategoria(@Path("usuarioId") usuarioId: Long, @Path("categoriaId") categoriaId: Long, @Body categoria: CategoriaDto): CategoriaDto
 
     @DELETE("api/categorias/{usuarioId}/{categoriaId}")
     suspend fun deleteCategoria(@Path("usuarioId") usuarioId: Long, @Path("categoriaId") categoriaId: Long)
