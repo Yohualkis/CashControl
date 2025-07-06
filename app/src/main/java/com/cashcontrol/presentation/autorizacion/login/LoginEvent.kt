e443c842fee947009938345baacdd3a1c4d17460
Yohualkis <Yohualkis99@gmail.com>
2025-07-06 00:28:18 -0400

    feat(presentation/auth/login): Se creoaron los eventos del login

---
+++ app/src/main/java/com/cashcontrol/presentation/autorizacion/login/LoginEvent.kt
@@ -0,0 +1,10 @@
+package com.cashcontrol.presentation.autorizacion.login
+
+sealed interface LoginEvent {
+    data class EmailChange(val email: String): LoginEvent
+    data class ContrasenaChange(val contrasena: String): LoginEvent
+
+    data object Login: LoginEvent
+    data object LimpiarErrores: LoginEvent
+    data object LimpiarClaveYErrorContrasena: LoginEvent
+}
