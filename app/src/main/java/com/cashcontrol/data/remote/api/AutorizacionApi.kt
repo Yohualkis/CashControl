c6a01f6a168c5b8d13e6ad70ff75af80e4727e22
Yohualkis <Yohualkis99@gmail.com>
2025-07-06 00:23:14 -0400

    refactor(di): se elimino el retorno Response<T> de ambos metodos

--- app/src/main/java/com/cashcontrol/data/remote/api/AutorizacionApi.kt
+++ app/src/main/java/com/cashcontrol/data/remote/api/AutorizacionApi.kt
@@ -3,14 +3,14 @@ package com.cashcontrol.data.remote.api
 import com.cashcontrol.data.remote.dto.AutorizacionResponseDto
 import com.cashcontrol.data.remote.dto.LoginRequestDto
 import com.cashcontrol.data.remote.dto.RegisterRequestDto
-import retrofit2.Response
+import com.cashcontrol.data.remote.dto.UsuarioDto
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AutorizacionApi {
     @POST("api/autorizacion/login")
-    suspend fun login(@Body request: LoginRequestDto): Response<AutorizacionResponseDto>
+    suspend fun login(@Body request: LoginRequestDto): AutorizacionResponseDto
 
     @POST("api/autorizacion/register")
-    suspend fun register(@Body request: RegisterRequestDto): Response<AutorizacionResponseDto>
+    suspend fun register(@Body usuarioDto: UsuarioDto): UsuarioDto
 }
