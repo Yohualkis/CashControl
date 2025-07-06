7bab40e3ece68b4f576fda4659a6087d704c93e3
Yohualkis <Yohualkis99@gmail.com>
2025-07-06 00:24:22 -0400

    fix(repositories): se agregaron los Json para claridad

--- app/src/main/java/com/cashcontrol/data/remote/dto/AutorizacionResponseDto.kt
+++ app/src/main/java/com/cashcontrol/data/remote/dto/AutorizacionResponseDto.kt
@@ -1,6 +1,10 @@
 package com.cashcontrol.data.remote.dto
 
+import com.squareup.moshi.Json
+
 data class AutorizacionResponseDto(
+    @Json(name = "token")
     val token: String,
+    @Json(name = "usuario")
     val usuario: UsuarioDto
 )
