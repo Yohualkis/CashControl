49b7fc9c196f5c3f04f833a92d58db3840170617
Yohualkis <Yohualkis99@gmail.com>
2025-07-15 03:17:02 -0400

    refactor(login): Se actualizo los valores por defecto a null en LoginUiState

--- app/src/main/java/com/cashcontrol/presentation/autorizacion/login/LoginUiState.kt
+++ app/src/main/java/com/cashcontrol/presentation/autorizacion/login/LoginUiState.kt
@@ -3,10 +3,10 @@ package com.cashcontrol.presentation.autorizacion.login
 import com.cashcontrol.data.local.entities.UsuarioEntity
 
 data class LoginUiState(
-    val email: String? = "",
-    val contrasena: String? = "",
-    val emailErrorMessage: String? = "",
-    val contrasenaErrorMessage: String? = "",
+    val email: String? = null,
+    val contrasena: String? = null,
+    val emailErrorMessage: String?  = null,
+    val contrasenaErrorMessage: String? = null,
     val isLoading: Boolean = false,
     val isLoggedIn: Boolean = false,
     val errorGeneral: String? = null,
