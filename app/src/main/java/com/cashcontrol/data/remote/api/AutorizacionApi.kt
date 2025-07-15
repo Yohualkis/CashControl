5cc55dacf432e4252b924afba4965acf18e2bc7c
Yohualkis <Yohualkis99@gmail.com>
2025-07-15 03:21:21 -0400

    refactor(api):Se modificaron los endpoints de login y register

--- app/src/main/java/com/cashcontrol/data/remote/api/AutorizacionApi.kt
+++ app/src/main/java/com/cashcontrol/data/remote/api/AutorizacionApi.kt
@@ -1,15 +1,15 @@
 package com.cashcontrol.data.remote.api
 
 import com.cashcontrol.data.remote.dto.AutorizacionResponseDto
-import com.cashcontrol.data.remote.dto.LoginRequestDto
-import com.cashcontrol.data.remote.dto.UsuarioDto
+import com.cashcontrol.data.remote.dto.AutorizacionRequestDto
+import com.cashcontrol.data.remote.dto.UsuarioRequestDto
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AutorizacionApi {
-    @POST("api/autorizacion/login")
-    suspend fun login(@Body request: LoginRequestDto): AutorizacionResponseDto
+    @POST("api/auth/login")
+    suspend fun login(@Body request: AutorizacionRequestDto): AutorizacionResponseDto
 
-    @POST("api/autorizacion/register")
-    suspend fun register(@Body usuarioDto: UsuarioDto): UsuarioDto
+    @POST("api/usuarios/register")
+    suspend fun register(@Body usuarioRequestDto: UsuarioRequestDto): UsuarioRequestDto
 }
