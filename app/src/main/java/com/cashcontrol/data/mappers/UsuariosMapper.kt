2f4fa34ede602eb78b13d12cb448fc4d99e1ccad
Yohualkis <Yohualkis99@gmail.com>
2025-07-15 02:44:12 -0400

    refactor(mappers): Se agregaron varias funciones para mapear los users

--- app/src/main/java/com/cashcontrol/data/mappers/UsuariosMapper.kt
+++ app/src/main/java/com/cashcontrol/data/mappers/UsuariosMapper.kt
@@ -1,14 +1,29 @@
 package com.cashcontrol.data.mappers
 
 import com.cashcontrol.data.local.entities.UsuarioEntity
-import com.cashcontrol.data.remote.dto.UsuarioDto
+import com.cashcontrol.data.remote.dto.UsuarioRequestDto
+import com.cashcontrol.data.remote.dto.UsuarioResponseDto
 
-fun UsuarioDto.toEntity(): UsuarioEntity {
-    return UsuarioEntity(
-        usuarioId = usuarioId,
-        nombre = nombre ?: "",
-        email = email ?: "",
-        fotoPath = fotoPath,
-        fechaRegistro = fechaRegistro
-    )
-}
+fun UsuarioRequestDto.toEntity() = UsuarioEntity(
+    usuarioId = usuarioId,
+    nombre = nombre ?: "",
+    email = email ?: "",
+    fotoPath = fotoPath,
+    fechaRegistro = null
+)
+
+fun UsuarioResponseDto.toEntity() = UsuarioEntity(
+    usuarioId = usuarioId,
+    nombre = nombre ?: "",
+    email = email ?: "",
+    fotoPath = fotoPath,
+    fechaRegistro = fechaRegistro
+)
+
+fun UsuarioEntity.toResponse() = UsuarioResponseDto(
+    usuarioId = this.usuarioId,
+    nombre = this.nombre,
+    email = this.email,
+    fechaRegistro = this.fechaRegistro,
+    fotoPath = this.fotoPath
+)
