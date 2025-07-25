a0d764c8237bfe39a3007f9bd7087801723eb23c
Yohualkis <Yohualkis99@gmail.com>
2025-07-24 23:12:30 -0400

    refactor(api): se cambio un endpoint y el nombre de un parametro

--- app/src/main/java/com/cashcontrol/data/remote/api/CategoriaApi.kt
+++ app/src/main/java/com/cashcontrol/data/remote/api/CategoriaApi.kt
@@ -2,26 +2,31 @@ package com.cashcontrol.data.remote.api
 
 import com.cashcontrol.data.remote.dto.CategoriaRequestDto
 import com.cashcontrol.data.remote.dto.CategoriaResponseDto
+import com.cashcontrol.data.remote.dto.FiltroCategoriaRequestDto
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CategoriaApi {
     @GET("api/categorias")
-    suspend fun getCategorias(@Body categoriaRequest: CategoriaRequestDto): List<CategoriaResponseDto>
+    suspend fun getCategorias(): List<CategoriaResponseDto>
 
     @GET("api/categorias/{categoriaId}")
     suspend fun getCategoriaById(@Path("categoriaId") categoriaId: Long): CategoriaResponseDto
 
+    @POST("api/categorias/tipo-y-usuario")
+    suspend fun getCategoriaPorUsuarioYTipo(@Body request: FiltroCategoriaRequestDto): List<CategoriaResponseDto>
+
     @POST("api/categorias")
     suspend fun postCategoria(@Body categoriaRequest: CategoriaRequestDto): CategoriaResponseDto
 
-    @PUT("api/categorias/{categoriaId}")
-    suspend fun putCategoria(@Body categoria: CategoriaResponseDto): CategoriaResponseDto
+    @PUT("api/categorias")
+    suspend fun putCategoria(@Body request: CategoriaRequestDto): CategoriaResponseDto
 
     @DELETE("api/categorias/{categoriaId}")
-    suspend fun deleteCategoria(@Path("categoriaId") categoriaId: Long)
+    suspend fun deleteCategoria(@Path("categoriaId") categoriaId: Long?): Boolean
 }
