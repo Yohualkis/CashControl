85d0703b6b944526dccbd81d892d3180bdb13b19
Yohualkis <Yohualkis99@gmail.com>
2025-07-24 23:11:53 -0400

    refactor(navigation): se agrego un scaffold para usar la barra de navegacion en las pantallas

--- app/src/main/java/com/cashcontrol/presentation/navigation/CashControlNavHost.kt
+++ app/src/main/java/com/cashcontrol/presentation/navigation/CashControlNavHost.kt
@@ -1,56 +1,113 @@
 package com.cashcontrol.presentation.navigation
 
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.cashcontrol.presentation.autorizacion.login.LoginScreen
 import com.cashcontrol.presentation.autorizacion.register.RegisterScreen
+import com.cashcontrol.presentation.categoria.CategoriaListScreen
+import com.cashcontrol.presentation.categoria.CategoriaScreen
+import com.cashcontrol.presentation.composables.AppBottomBar
 import com.cashcontrol.presentation.dashboard.DashboardScreen
 
 @Composable
 fun CashControlNavHost(
     nav: NavHostController,
 ) {
-    NavHost(
-        navController = nav,
-        startDestination = Screen.Login
-    ) {
-
-        // LOGIN
-        composable<Screen.Login> {
-            LoginScreen(
-                goToDash = {
-                    nav.navigate(Screen.Dashboard) {
-                        popUpTo(Screen.Login) { inclusive = true }
-                    }
-                },
-                goToRegister = {
-                    nav.navigate(Screen.Register){
-                        popUpTo(Screen.Login) { inclusive = true }
-                    }
-                },
-            )
+    val currentScreen by remember {
+        derivedStateOf {
+            when (nav.currentDestination?.route?.substringBefore("/")) {
+                "dashboard" -> Screen.Dashboard
+                "categorias" -> Screen.ListaCategorias
+                "sugerencias" -> Screen.Sugerencias
+                else -> Screen.Dashboard
+            }
         }
-
-        // REGISTER
-        composable<Screen.Register> {
-            RegisterScreen(
-                goToLogin = {
-                    nav.navigate(Screen.Login){
-                        popUpTo(Screen.Register) { inclusive = true }
-                    }
-                }
-            )
+    }
+    Scaffold(
+        bottomBar = {
+            // Solo muestra en pantallas principales
+            when (currentScreen) {
+                is Screen.Login, is Screen.Register -> Unit // No mostrar
+                else -> AppBottomBar(
+                    pantallaActual = currentScreen,
+                    onNavigate = { screen ->
+                        when (screen) {
+                            is Screen.Dashboard -> nav.navigate(Screen.Dashboard)
+                            is Screen.ListaCategorias -> nav.navigate(Screen.ListaCategorias)
+                            is Screen.Sugerencias -> nav.navigate(Screen.Sugerencias)
+                            else -> {}
+                        }
+                    },
+                )
+            }
         }
+    ) { innerPadding ->
+        NavHost(
+            navController = nav,
+            startDestination = Screen.Login,
+            modifier = Modifier.padding(innerPadding)
+        ) {
 
-        // DASHBOARD
-        composable<Screen.Dashboard> {
-            DashboardScreen(
-                goToDashboard = {},
-                goToSugerencias = {}
-            )
+            // LOGIN
+            composable<Screen.Login> {
+                LoginScreen(
+                    goToDash = {
+                        nav.navigate(Screen.Dashboard) {
+                            popUpTo(Screen.Login) { inclusive = true }
+                        }
+                    },
+                    goToRegister = {
+                        nav.navigate(Screen.Register) {
+                            popUpTo(Screen.Login) { inclusive = true }
+                        }
+                    },
+                )
+            }
+
+            // REGISTER
+            composable<Screen.Register> {
+                RegisterScreen(
+                    goToLogin = {
+                        nav.navigate(Screen.Login) {
+                            popUpTo(Screen.Register) { inclusive = true }
+                        }
+                    }
+                )
+            }
+
+            // DASHBOARD
+            composable<Screen.Dashboard> {
+                DashboardScreen(
+                    goToDashboard = {},
+                    goToSugerencias = {},
+                )
+            }
+
+            // LISTA DE CATEGORIAS
+            composable<Screen.ListaCategorias> {
+                CategoriaListScreen(
+                    goToCategoria = { nav.navigate(Screen.Categoria(it)) },
+                    goBack = { nav.navigateUp() },
+                )
+            }
+
+            // CATEGORIA
+            composable<Screen.Categoria> { backStack ->
+                val args = backStack.toRoute<Screen.Categoria>()
+                CategoriaScreen(
+                    categoriaId = args.categoriaId,
+                    goBack = { nav.navigateUp() },
+                )
+            }
         }
     }
 }
