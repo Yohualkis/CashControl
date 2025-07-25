d06e837ff960af610cb4205d752db5d9cbf587ea
Yohualkis <Yohualkis99@gmail.com>
2025-07-24 23:08:14 -0400

    refactor(di): modifique el api module

--- app/src/main/java/com/cashcontrol/di/ApiModule.kt
+++ app/src/main/java/com/cashcontrol/di/ApiModule.kt
@@ -2,16 +2,12 @@ package com.cashcontrol.di
 
 import com.cashcontrol.data.remote.api.AutorizacionApi
 import com.cashcontrol.data.remote.api.CategoriaApi
-import com.cashcontrol.data.remote.interceptors.AutorizacionInterceptor
-import com.cashcontrol.session.Sesion
-import com.cashcontrol.util.TokenProvider
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -25,57 +21,29 @@ object ApiModule {
 
     @Provides
     @Singleton
-    fun providesMoshi(): Moshi =
+    fun provideMoshi(): Moshi =
         Moshi.Builder()
-            .add(DateAdapter())
             .add(KotlinJsonAdapterFactory())
+            .add(DateAdapter())
             .build()
 
     @Provides
     @Singleton
-    fun provideTokenProvider(): TokenProvider {
-        return TokenProvider {
-            Sesion.token
-        }
-    }
-
-    @Provides
-    @Singleton
-    fun provideAutorizacionInterceptor(tokenProvider: TokenProvider): AutorizacionInterceptor {
-        return AutorizacionInterceptor { tokenProvider.getToken() }
-    }
-
-    @Provides
-    @Singleton
-    fun provideOkHttpClient(authInterceptor: AutorizacionInterceptor): OkHttpClient {
-        return OkHttpClient.Builder()
-            .addInterceptor(authInterceptor)
-            .build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideRetrofit(client: OkHttpClient): Retrofit {
+    fun provideCategoriaApi(): CategoriaApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .client(client)
-            .addConverterFactory(MoshiConverterFactory.create(providesMoshi()))
-            .build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideAuthApi(retrofit: Retrofit): AutorizacionApi {
-        return retrofit.create(AutorizacionApi::class.java)
-    }
-
-    @Provides
-    @Singleton
-    fun provideCategoriaApi(moshi: Moshi): CategoriaApi {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
             .build()
             .create(CategoriaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAutorizacionApi(): AutorizacionApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(provideMoshi()))
+            .build()
+            .create(AutorizacionApi::class.java)
     }
 }
