404db9f4bf1a87dcf50dfc9980c8b4b18492ff99
Yohualkis <Yohualkis99@gmail.com>
2025-07-24 23:10:59 -0400

    refactor(database): se hizo la migracion

--- app/src/main/java/com/cashcontrol/data/local/database/CashControlDb.kt
+++ app/src/main/java/com/cashcontrol/data/local/database/CashControlDb.kt
@@ -27,7 +27,7 @@ import com.cashcontrol.di.DateAdapter
         SugerenciaEntity::class,
         SugerenciaDetalleEntity::class,
     ],
-    version = 2,
+    version = 1,
     exportSchema = false
 )
 @TypeConverters(DateAdapter::class)
