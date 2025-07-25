e484c2d990fbdce5766b143e6389b41726e41a14
Yohualkis <Yohualkis99@gmail.com>
2025-07-25 11:00:31 -0400

    refactor(remote): Se crearon los metodos de categoria

--- app/src/main/java/com/cashcontrol/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/cashcontrol/data/remote/RemoteDataSource.kt
@@ -2,8 +2,9 @@ package com.cashcontrol.data.remote
 
 import com.cashcontrol.data.remote.api.AutorizacionApi
 import com.cashcontrol.data.remote.api.CategoriaApi
-import com.cashcontrol.data.remote.dto.CategoriaResponseDto
 import com.cashcontrol.data.remote.dto.AutorizacionRequestDto
+import com.cashcontrol.data.remote.dto.CategoriaRequestDto
+import com.cashcontrol.data.remote.dto.FiltroCategoriaRequestDto
 import com.cashcontrol.data.remote.dto.UsuarioRequestDto
 import javax.inject.Inject
 
@@ -12,7 +13,17 @@ class RemoteDataSource @Inject constructor(
     private val autorizacionApi: AutorizacionApi
 ) {
     // CATEGORIAS
+    suspend fun crearCategoria(requestCategoriaDto: CategoriaRequestDto) =
+        categoriaApi.postCategoria(requestCategoriaDto)
 
+    suspend fun editarCategoria(requestCategoriaDto: CategoriaRequestDto) =
+        categoriaApi.putCategoria(requestCategoriaDto)
+
+    suspend fun eliminarCategoria(id: Long?) =
+        categoriaApi.deleteCategoria(id)
+
+    suspend fun getCategoriaPorUsuarioYTipo(request: FiltroCategoriaRequestDto) =
+        categoriaApi.getCategoriaPorUsuarioYTipo(request)
 
     // AUTORIZACION
     suspend fun login(loginRequest: AutorizacionRequestDto) = autorizacionApi.login(loginRequest)
