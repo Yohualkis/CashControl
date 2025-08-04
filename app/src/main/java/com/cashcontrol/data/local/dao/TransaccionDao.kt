1fa8b2dfcfbee6e769fcb1c3fe25dc6a28a65db2
Yohualkis <Yohualkis99@gmail.com>
2025-08-03 23:33:32 -0400

    refactor(TransaccionDao): Se agrega método para guardar lista de transacciones y obtener transacciones por usuario y categoría
    Se han realizado los siguientes cambios en la interfaz `TransaccionDao`:
    - Se agregó la función `saveLista` para guardar una lista de `TransaccionEntity` utilizando la anotación `@Upsert`.
    - Se eliminó la función `getAll` que devolvía un `Flow` de `List<TransaccionEntity?>`.
    - Se agregó la función `getTransaccionesPorUsuarioYCategoria` para obtener una lista de `TransaccionEntity` filtrando por `usuarioId` y `tipoCategoria`.

--- app/src/main/java/com/cashcontrol/data/local/dao/TransaccionDao.kt
+++ app/src/main/java/com/cashcontrol/data/local/dao/TransaccionDao.kt
@@ -5,12 +5,14 @@ import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
 import com.cashcontrol.data.local.entities.TransaccionEntity
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 sealed interface TransaccionDao {
     @Upsert
     suspend fun save(transaccion: TransaccionEntity)
+
+    @Upsert
+    suspend fun saveLista(listaTransacciones: List<TransaccionEntity>)
 
     @Query("""
         select *
@@ -23,6 +25,16 @@ sealed interface TransaccionDao {
     @Delete
     suspend fun delete(transaccion: TransaccionEntity)
 
-    @Query("select * from Transacciones")
-    fun getAll(): Flow<List<TransaccionEntity?>>
+    @Query("""
+        select *
+        from Transacciones t
+        inner join Categorias c on t.categoriaId = c.categoriaId
+        where t.usuarioId = :usuarioId
+        and c.usuarioId = :usuarioId
+        and c.tipo = :tipoCategoria
+        """)
+    suspend fun getTransaccionesPorUsuarioYCategoria(
+        usuarioId: Long,
+        tipoCategoria: String
+    ): List<TransaccionEntity>
 }
