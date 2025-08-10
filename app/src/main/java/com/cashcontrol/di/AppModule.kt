f359fbfb420804107af89af2bf5607cbff557156
Yohualkis <Yohualkis99@gmail.com>
2025-08-10 13:00:00 -0400

    refactor(di): Enable fallback to destructive migration
    The `fallbackToDestructiveMigration` method is now called with `true` to allow Room to destructively recreate database tables if migrations are not available.

--- app/src/main/java/com/cashcontrol/di/AppModule.kt
+++ app/src/main/java/com/cashcontrol/di/AppModule.kt
@@ -3,8 +3,7 @@ package com.cashcontrol.di
 import android.content.Context
 import androidx.room.Room
 import com.cashcontrol.data.local.database.CashControlDb
-import dagger.Module
-import dagger.Provides
+import dagger.*
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
@@ -20,7 +19,7 @@ object AppModule {
             context = contexto,
             klass = CashControlDb::class.java,
             name = "CashControl.db"
-        ).fallbackToDestructiveMigration()
+        ).fallbackToDestructiveMigration(true)
             .build()
 
     @Provides
