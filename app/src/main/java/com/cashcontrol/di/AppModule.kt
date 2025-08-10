50f4770d32c08308913efeda4a980ba025db3893
Yohualkis <Yohualkis99@gmail.com>
2025-08-10 12:23:46 -0400

    refactor: Se unicaron 2 imports y se dejo de utilizar un metodo deprecado

--- app/src/main/java/com/cashcontrol/di/AppModule.kt
+++ app/src/main/java/com/cashcontrol/di/AppModule.kt
@@ -3,8 +3,7 @@ package com.cashcontrol.di
 import android.content.Context
 import androidx.room.Room
 import com.cashcontrol.data.local.database.CashControlDb
-import dagger.Module
-import dagger.Provides
+import dagger.*
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
@@ -17,10 +16,10 @@ object AppModule {
     @Singleton
     fun provideCashControlDb(@ApplicationContext contexto: Context) =
         Room.databaseBuilder(
-            context = contexto,
-            klass = CashControlDb::class.java,
-            name = "CashControl.db"
-        ).fallbackToDestructiveMigration()
+                context = contexto,
+                klass = CashControlDb::class.java,
+                name = "CashControl.db"
+            ).fallbackToDestructiveMigration(true)
             .build()
 
     @Provides
